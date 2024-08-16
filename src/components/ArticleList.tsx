9434393df9a3bf9f742a33b93cbbc49399ebe494
Corey Chiu <qiuyu@qiuyu.dev>
2024-08-17 01:46:47 +0800

    update search

--- src/components/ArticleList.tsx
+++ src/components/ArticleList.tsx
@@ -19,7 +19,7 @@ const ArticleList = ({ articles, showMoreLink = true }) => {
       <div className="flex justify-between items-center mb-6">
         <h2 className="text-3xl font-bold tracking-tight">Articles</h2>
         {showMoreLink && (
-          <Link href="/posts" className="text-blue-600 hover:text-blue-800 transition-colors">
+          <Link href="/article" className="text-blue-600 hover:text-blue-800 transition-colors">
             More articles →
           </Link>
         )}
@@ -30,7 +30,7 @@ const ArticleList = ({ articles, showMoreLink = true }) => {
           <Card key={id}>
             <CardHeader>
               <Link 
-                href={`/posts/${id}`}
+                href={`/article/${id}`}
                 className="text-blue-600 hover:text-blue-800 transition-colors inline-flex items-center gap-1"
               >
                 <CardTitle className='mr-1'>{title}</CardTitle>
@@ -49,15 +49,9 @@ const ArticleList = ({ articles, showMoreLink = true }) => {
 const ArticlePage = ({ articles }) => {
   return (
     <section>
-      {/* Breadcrumb navigation */}
-      <nav className="flex items-center text-sm text-gray-500 mb-6">
-          <Link href="/" className="hover:text-blue-600">Home</Link>
-          <ChevronRightIcon className="mx-2" size={16} />
-          <span className="text-gray-900 capitalize">Articles</span>
-        </nav>
       <div className="flex flex-col justify-between items-center mb-6">
         <h1 className="mx-auto max-w-3xl text-3xl font-bold lg:text-5xl tracking-tight">
-          <span className="text-primary">Articles</span>
+          <span className="text-primary pt-10">Article</span>
         </h1>
         <h2 className="mx-auto max-w-[700px] text-gray-500 md:text-xl my-6">Read More About DevToolset</h2>
       </div>
@@ -67,7 +61,7 @@ const ArticlePage = ({ articles }) => {
           <Card key={id}>
             <CardHeader>
               <Link 
-                href={`/posts/${id}`}
+                href={`/article/${id}`}
                 className="text-blue-600 hover:text-blue-800 transition-colors inline-flex items-center gap-1"
               >
                 <CardTitle className='mr-1'>{title}</CardTitle>
