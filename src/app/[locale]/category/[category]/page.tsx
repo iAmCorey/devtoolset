960dcf7602f12c7c20908ec93d8ff29fe99f9263
Corey Chiu <iamcoreychiu@gmail.com>
2024-08-19 09:47:10 +0800

    add srcs

--- src/app/[locale]/category/[category]/page.tsx
+++ src/app/[locale]/category/[category]/page.tsx
@@ -62,9 +62,13 @@ export default async function Tool({ params: { category } }: CategoryPageProps) 
       </Breadcrumb>
       <div className="flex flex-col justify-between items-center mb-12">
         <h1 className="text-3xl font-bold tracking-tight capitalize lg:text-5xl pt-10">{categoryData.name}</h1>
-        <h2 className='text-sm mt-2 opacity-60 lg:text-lg'>{t('h2description')}</h2>
+        <h2 className="text-md mt-2 opacity-60 lg:text-xl">{categoryData.description}</h2>
+        
       </div>
       <ToolsPage category={categoryData} locale={locale} />
+      <div className="flex flex-col justify-between items-center mt-12">
+        <h2 className='text-sm mt-2 opacity-60 lg:text-md'>{t('h2description')}</h2>
+      </div>
     </div>
   )
 }
